6b2abd5259dde87f1fb8682cec3b609184c537eb
Oleg Seriaga <oleg.seriaga@gmail.com>
2022-01-12 14:01:02 +0200

    error route fix

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
-
 const app = express();
 
 app.use(logger('dev'));
@@ -13,20 +12,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.json({});
 });
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({message: err.message, code: err.status || 500});
 });
 
 module.exports = app;
