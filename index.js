098bb4722fb661fc00474a169c19f7b775cdbd92
Oleg Seriaga <oleg.seriaga@gmail.com>
2022-01-14 16:08:56 +0200

    change port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require(`./app`);
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, (err) => {
     if (err) {
         return console.log(err.message);
