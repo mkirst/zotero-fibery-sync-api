7b823575b3ab83e9f219035c3098887c0de346f1
Oleg Seriaga <oleg.seriaga@gmail.com>
2022-01-14 14:21:03 +0200

    implement app

--- app.js
+++ app.js
@@ -1,25 +1,79 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
+const express = require(`express`);
+const path = require(`path`);
+const logger = require(`morgan`);
+const appConfig = require(`./config.app.json`);
+const syncConfig = require(`./config.sync.json`);
+const schema = require(`./schema.json`);
+const wrap = require(`express-async-wrap`);
+const _ = require(`lodash`);
+const uuid = require(`uuid-by-string`);
+const got = require(`got`);
+
+const getYearRange = filter => {
+    const yearRange = [filter.from, filter.to];
+
+    if (!_.isNumber(yearRange[0])) {
+        yearRange[0] = new Date().getFullYear() - 1;
+    }
+
+    if (!_.isNumber(yearRange[1])) {
+        yearRange[1] = new Date().getFullYear();
+    }
+    return yearRange;
+};
+
 const app = express();
-
-app.use(logger('dev'));
+app.use(logger(`dev`));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, `resources`)));
 
-app.get('/', (req, res) => {
-  res.json({});
+app.get(`/`, (req, res) => {
+    res.json(appConfig);
 });
 
-app.use(function(req, res, next) {
-  next(createError(404));
+app.post(`/api/v1/synchronizer/config`, (req, res) => {
+    res.json(syncConfig);
 });
 
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({message: err.message, code: err.status || 500});
+app.post(`/api/v1/synchronizer/schema`, (req, res) => {
+    res.json(schema);
+});
+
+app.post(`/api/v1/synchronizer/data`, wrap(async (req, res) => {
+    const {requestedType, filter} = req.body;
+    if (requestedType !== `holidays`) {
+        throw new Error(`Only holidays database can be synchronized`);
+    }
+    if (_.isEmpty(filter.countries)) {
+        throw new Error(`Countries filter should be specified`);
+    }
+    const {countries} = filter;
+    const yearRange = getYearRange(filter);
+    const items = [];
+    for (const country of countries) {
+        for (const year of yearRange) {
+            const url = `https://date.nager.at/api/v3/PublicHolidays/${year}/${country}`;
+            console.log(url);
+            (await (got(url).json())).forEach((item) => {
+                    item.id = uuid(JSON.stringify(item));
+                    items.push(item);
+                });
+        }
+    }
+    return res.json({items});
+}));
+
+app.use(function (req, res, next) {
+    const error = new Error(`Not found`);
+    error.status = 404;
+    next(error);
+});
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    console.log(err);
+    res.json({message: err.message, code: err.status || 500});
 });
 
 module.exports = app;
--- test.js
+++ test.js
@@ -1,8 +1,42 @@
-const request = require('supertest');
-const app = require('./app');
+const request = require(`supertest`);
+const app = require(`./app`);
+const assert = require(`assert`);
 
-describe('integration app suite', function () {
-    it('should have the logo', async () => {
-        await request(app).get('/logo.svg').expect(200);
+describe(`integration app suite`, function () {
+    it(`should have the logo`, async () => {
+        await request(app).get(`/logo.svg`).expect(200).expect(`Content-Type`, /svg/);
+    });
+
+    it(`should have app config`, async () => {
+        const {body: appConfig} = await request(app).get(`/`)
+            .expect(200).expect(`Content-Type`, /json/);
+
+        assert.equal(appConfig.name, `Public Holidays`);
+        assert.equal(appConfig.version, `1.0.0`);
+        assert.match(appConfig.description, /public holidays/);
+        assert.equal(appConfig.responsibleFor.dataSynchronization, true);
+    });
+
+    it(`should have synchronization config`, async () => {
+        const {body: {types, filters}} = await request(app).post(`/api/v1/synchronizer/config`)
+            .expect(200).expect(`Content-Type`, /json/);
+        assert.equal(types.length, 1);
+        assert.equal(filters.length, 3);
+    });
+
+    it(`should have schema holidays type defined`, async () => {
+        const {body: {schema: {holidays}}} = await request(app).post(`/api/v1/synchronizer/schema`)
+            .expect(200).expect(`Content-Type`, /json/);
+        assert.deepEqual(holidays.id, {name: `Id`, type: `id`});
+    });
+
+    it(`should return data for CY`, async () => {
+        const {body: {items}} = await request(app).post(`/api/v1/synchronizer/data`).send({
+            requestedType: `holidays`,
+            filter: {
+                countries: [`CY`]
+            }
+        }).expect(200).expect(`Content-Type`, /json/);
+        assert.equal(items.length > 0, true);
     });
 });
