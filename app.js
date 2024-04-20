93d6b1428a24eea56c757ff2337ade8bcc02c5bf
mkirst <mkirst@users.noreply.github.com>
2024-04-20 21:02:09 +0200

    Update app.js
    Another try with gloabl bibKeys

--- app.js
+++ app.js
@@ -115,8 +115,10 @@ const schema = require(`./schema.json`);
 
 app.post(`/api/v1/synchronizer/schema`, (req, res) => res.json(schema));
 
+let bibKeys = [];
+
 app.post(`/api/v1/synchronizer/data`, wrap(async (req, res) => {
-    let {requestedType, pagination, account, lastSynchronizedAt, bibKeys} = req.body;
+    let {requestedType, pagination, account, lastSynchronizedAt} = req.body;
     const req_opts = {headers:{}};
     if (account.auth == "token") {
         req_opts.headers["Zotero-API-Key"] = account.token;
@@ -148,8 +150,6 @@ app.post(`/api/v1/synchronizer/data`, wrap(async (req, res) => {
     }
 
     let items = [];
-    //let bibKeys = [];
-
 
     if (pagination != null && pagination["link"] != null) {
         console.log("using pagination link ", pagination["link"], "for type", requestedType);
